aec4b50b3d40de3185ef5aeba19289f7bf19558e
whatisthelemon ph <whatisthelemon@gmail.com>
2015-03-14 18:22:25 -0400

    Finished part 1 of the HW (hopefully). NOTE: find a way to make the code more elegant.

--- hw1/pollutantmean.R
+++ hw1/pollutantmean.R
@@ -13,7 +13,22 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
     ## in the 'id' vector (ignoring NA values)
 
     #open from directory
+    data <- extractmulticsv(directory)
+    
+    #figure out which ids
+    onestoselectfrom <- data$ID %in% id
+    
+    #extract mean
+    return (mean(data[, pollutant][onestoselectfrom], na.rm=TRUE))
+}
+
+extractmulticsv <- function(directory="."){
     filenames <- list.files(path=directory)
     filenames <- paste(directory, "/", filenames, sep="")
-    data <- cbind(lapply(filenames, read.csv))
+    data <- read.csv(filenames[1])
+    for(i in 2:length(filenames)){
+        data <- rbind(data, read.csv(filenames[i]))
+    }
+    return (data)
 }
+    
