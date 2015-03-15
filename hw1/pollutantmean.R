ee6698456411bf746de8dbfb3b475c9073a3c885
whatisthelemon ph <whatisthelemon@gmail.com>
2015-03-15 12:31:58 -0400

    Done with part 2 of hw1

--- hw1/pollutantmean.R
+++ hw1/pollutantmean.R
@@ -22,6 +22,34 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
     return (mean(data[, pollutant][onestoselectfrom], na.rm=TRUE))
 }
 
+complete <- function(directory, id = 1:332) {
+    ## 'directory' is a character vector of length 1 indicating
+    ## the location of the CSV files
+    
+    ## 'id' is an integer vector indicating the monitor ID numbers
+    ## to be used
+    
+    ## Return a data frame of the form:
+    ## id nobs
+    ## 1  117
+    ## 2  1041
+    ## ...
+    ## where 'id' is the monitor ID number and 'nobs' is the
+    ## number of complete cases
+    data <- extractmulticsv(directory)
+    completes <- complete.cases(data)
+    
+    nobs <- vector('integer')
+    
+    for(i in id){
+        temp <- data$ID==i & completes
+        s <- sum(temp)
+        nobs <- c(nobs, s)
+    }
+    
+    return(data.frame(id, nobs))
+}
+
 extractmulticsv <- function(directory="."){
     filenames <- list.files(path=directory)
     filenames <- paste(directory, "/", filenames, sep="")
